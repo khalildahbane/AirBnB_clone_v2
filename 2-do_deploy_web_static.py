01afd5227e27f7696265fb46ef951bd07b255a47
kelldani <kelldani100@gmail.com>
2024-02-07 10:51:01 -0800

    AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,46 @@
-dsojf
+#!/usr/bin/python3
+""" Deploy archive!
+"""
+from fabric.api import task, local, env, put, run
+from datetime import datetime
+import os
+
+env.hosts = ['54.162.34.11', '100.25.140.43']
+
+
+@task
+def do_pack():
+    """ do_pack method
+    """
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
+
+
+@task
+def do_deploy(archive_path):
+    """ do_deploy method
+    """
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
